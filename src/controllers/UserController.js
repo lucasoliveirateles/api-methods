de4b2850d81e4e6d862ae4116f1514d14913eab3
Lucas Teles <dev.teles@outlook.com>
2024-05-11 20:24:14 -0300

    put try catch on user controller in method get

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -2,9 +2,13 @@ import api from '../services/api.js';
 
 class UserController {
   async get(request, response) {
-    const axios = await api.get('/data');
+    try {
+      const axios = await api.get('/data');
     
-    return response.json(axios.data);
+      return response.json(axios.data);  
+    } catch (error) {
+      return response.status(500).json({ server: 'error' }); 
+    }
   }
 
   async post(request, response) {
